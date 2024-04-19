f4f5b0d3c4824c61fb99fc5ba0a7a02c949dadad
prometheusowner <Barrygao2001@outlook.com>
2024-04-19 15:03:23 -0600

    merge

---
+++ datasets/PersonCar/label_name.py
@@ -0,0 +1,3 @@
+English_name = {'person': "people", 'bicycle': 'bicycle', 'car': 'car', 'motorcycle': 'motorcycle',
+                      'bus': 'bus', 'truck': 'truck', 'train': 'train'}
+Label_list = list(English_name.values())
